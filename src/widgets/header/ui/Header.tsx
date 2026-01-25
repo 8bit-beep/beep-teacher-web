bc86a31337fe61d00ff6ae94cc6f603f625e1bf7
김태우 <taewoo080401@gmail.com>
2026-01-25 10:53:35 +0900

    fix: hidden header

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -8,7 +8,7 @@ import { Suspense } from "react";
 
 const Header = () => {
   return (
-    <header className="w-full flex items-center gap-4 my-4 px-2">
+    <header className="w-full flex items-center gap-4 my-4 px-2 xl:hidden">
       <Image width={40} height={40} src="/logo.svg" alt="logo" />
       <ManageMemo />
       <div className="flex-1" />
