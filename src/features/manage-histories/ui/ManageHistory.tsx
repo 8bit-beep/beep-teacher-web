ddd13e6fcbc6b03e5b64fa4fecf01c55c3926311
민선영 <ttt4306@gmail.com>
2026-02-27 20:49:35 +0900

    feat: add manageHistory header

--- src/features/manage-histories/ui/ManageHistory.tsx
+++ src/features/manage-histories/ui/ManageHistory.tsx
@@ -23,6 +23,13 @@ const ManageHistory = ({ room }: Props) => {
 
   return (
     <div className="w-full bg-static-white xl:rounded-l-large flex flex-col items-start slide-in-down">
+      <div className="w-full flex p-[10px]">
+        <p className="w-[180px] text-static-black text-body text-center">학생/이름</p>
+        <div className="flex-1"/>
+        <p className="w-[180px] text-static-black text-body text-center">8~9교시</p>
+        <p className="w-[180px] text-static-black text-body text-center">10~11교시</p>
+        <p className="w-[180px] text-static-black text-body text-center">최종</p>
+      </div>
         <Suspense
           fallback={
             <div className="w-full h-20 flex items-center justify-center text-greyscale-40">
