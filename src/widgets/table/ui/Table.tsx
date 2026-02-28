5c13b6b9397c6e7cc87b715feee3a6a123f9662f
민선영 <ttt4306@gmail.com>
2026-02-28 20:26:13 +0900

    feat: apply alternating row colors based on row index

--- src/widgets/table/ui/Table.tsx
+++ src/widgets/table/ui/Table.tsx
@@ -38,7 +38,7 @@ const Table = ({ header, rows }: Props) => {
               rows.map((row, rowIndex) => (
                 <tr
                   key={rowIndex}
-                  className="[&_td:first-child]:pl-2 xl:[&_td:first-child]:pl-10 [&_td:last-child]:pr-2 xl:[&_td:last-child]:pr-10">
+                  className={`[&_td:first-child]:pl-2 xl:[&_td:first-child]:pl-10 [&_td:last-child]:pr-2 xl:[&_td:last-child]:pr-10" ${rowIndex % 2 === 0 ? "bg-[#EFF8FF]" : "bg-white"}`}>
                   {row.map((cell, cellIndex) => (
                     <td
                       key={cellIndex}
@@ -46,7 +46,7 @@ const Table = ({ header, rows }: Props) => {
                       className="h-15 text-h4">
                       {cell}
                     </td>
-                  ))}
+                  ))}                
                 </tr>
               ))
             )}
