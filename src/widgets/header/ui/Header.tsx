8b718b86175fc857ff4ac78e39a9a449c93d1edb
민선영 <ttt4306@gmail.com>
2026-03-01 22:56:35 +0900

    fix: header style

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -8,13 +8,12 @@ import { Suspense } from "react";
 
 const Header = () => {
   return (
-    <header className="w-full flex items-center gap-2 my-4 px-[54px] xl:hidden">
+    <header className="w-full flex items-center gap-5 my-4 px-[54px] xl:hidden">
       <Image width={40} height={40} src="/logo.svg" alt="logo" />
-      <div className="w-full flex justify-between">
+      <div className="w-full min-w-0 flex justify-between">
         <div></div>
         <ManageMemo />
       </div>
-      <div className="flex-1" />
       <Suspense fallback={<SkeletonUser />}>
         <UserIndicator />
       </Suspense>
