b14615df283ce41173b78f6c397078b521c75880
민선영 <ttt4306@gmail.com>
2026-03-02 12:51:48 +0900

    fix: section style

--- src/widgets/section/ui/Section.tsx
+++ src/widgets/section/ui/Section.tsx
@@ -22,7 +22,7 @@ const Section = ({
 }: Props) => {
   return (
     <section className="w-full min-h-0 flex-1 bg-static-white pt-5 flex flex-col gap-5 rounded-large xl:rounded-b-none shadow-modal overflow-hidden">
-      <header className="h-11.5 flex items-center gap-2 xl:gap-4 px-10">
+      <header className="h-11.5 flex items-center gap-2 xl:gap-4 px-3 lg:px-10">
         <div className="text-static-dark">{icon}</div>
         <div className="flex-1 flex flex-col">
           <h2 className="text-accent lg:text-h3 xl:text-h2 text-static-black">
