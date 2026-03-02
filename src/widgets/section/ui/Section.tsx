cb144dc621ca3fadd2a8fd8eed5592dfe75e525b
민선영 <ttt4306@gmail.com>
2026-03-02 12:49:45 +0900

    fix: section style

--- src/widgets/section/ui/Section.tsx
+++ src/widgets/section/ui/Section.tsx
@@ -25,10 +25,10 @@ const Section = ({
       <header className="h-11.5 flex items-center gap-2 xl:gap-4 px-10">
         <div className="text-static-dark">{icon}</div>
         <div className="flex-1 flex flex-col">
-          <h2 className="xl:text-h2 text-h3 text-static-black">
+          <h2 className="text-accent lg:text-h3 xl:text-h2 text-static-black">
             {title}
           </h2>
-          <p className="xl:text-caption1 text-caption2 text-greyscale-40">
+          <p className="hidden lg:flex xl:text-caption1 text-caption2 text-greyscale-40">
             {description}
           </p>
         </div>
