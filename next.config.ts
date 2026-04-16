f4401533eec5aaf5356ccb5ac915f51b62256786
이윤지 <yoonji0879@dgsw.hs.kr>
2026-04-16 15:20:19 +0900

    chore: remove next/image remotePatterns config

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,6 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "standalone",
   devIndicators: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "dodamdodam-storage.s3.ap-northeast-2.amazonaws.com",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
