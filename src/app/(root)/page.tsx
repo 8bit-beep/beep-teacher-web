f1c93a29b5ec714aa97b2c22c781406d14f30e73
민선영 <ttt4306@gmail.com>
2026-08-31 15:36:40 +0900

    delete: 중복 새로고침

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -34,7 +34,6 @@ export default async function HomePage({
           <>
             <ScheduleButton />
             <ManageEvents />
-            <Refresh />
           </>
         }
         mobileFilter={<FloorFilterBar/>}>
