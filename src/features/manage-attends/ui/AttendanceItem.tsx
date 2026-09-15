f66b43c9c18592212173ee977c6afcef457f564b
민선영 <ttt4306@gmail.com>
2026-09-15 18:07:12 +0900

    style: 일괄 변경 UI 피그마 스펙 정밀 반영

--- src/features/manage-attends/ui/AttendanceItem.tsx
+++ src/features/manage-attends/ui/AttendanceItem.tsx
@@ -28,9 +28,6 @@ const AttendanceItem = ({ data, roomId, selected, onToggleSelect }: Props) => {
     <div
       className={`w-full h-15 flex items-center px-4 gap-2.5 min-[453px]:gap-4 border-b border-greyscale-20 min-[453px]:border-b-0 ${currentStatus ? (isAbsent ? "bg-red-light" : isOut ? "bg-green-light" : "bg-static-white") : "bg-greyscale-10"}`}>
       <Checkbox checked={selected} onChange={onToggleSelect} />
-      <div
-        className={`w-4.5 h-4.5 rounded-full shrink-0 ${data.isLate ? "bg-yellow-400" : "bg-transparent"}`}
-      />
       <p
         className={`text-caption1 min-[453px]:text-body inline-block w-9 ${isAbsent || isOut ? "text-greyscale-10" : "text-greyscale-40"}`}>
         {data.studentId}
@@ -39,6 +36,9 @@ const AttendanceItem = ({ data, roomId, selected, onToggleSelect }: Props) => {
         className={`text-body min-[453px]:text-h4 ${isAbsent || isOut ? "text-white" : "text-static-black"}`}>
         {data.name}
       </p>
+      {data.isLate && (
+        <div className="w-4.5 h-4.5 rounded-full shrink-0 bg-yellow-400" />
+      )}
       <div className="flex-1" />
       <Dropdown selected={status} onSelect={setStatus} options={options} width={"120px"}/>
     </div>
