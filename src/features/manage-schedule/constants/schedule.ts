5ba85f5b9feb4181a0ed4374d87bb007cf4d42ec
민선영 <ttt4306@gmail.com>
2026-09-21 19:22:09 +0900

    feat: 재정렬 모드에 교실자습 타입 추가

--- src/features/manage-schedule/constants/schedule.ts
+++ src/features/manage-schedule/constants/schedule.ts
@@ -1,4 +1,4 @@
 export const NO_CHANGE_VALUE = "none";
 export const NO_CHANGE_LABEL = "변경없음";
 
-export const SORT_MODE_TYPE_NAMES = ["나르샤", "동아리", "방과후"];
+export const SORT_MODE_TYPE_NAMES = ["나르샤", "동아리", "교실자습", "방과후"];
